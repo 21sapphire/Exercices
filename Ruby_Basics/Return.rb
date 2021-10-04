a5d72f6b2f94b5c7559e212eacee28aa4b9eeaeb
21 Sapphire <21sapphiremusic@gmail.com>
2021-10-04 08:20:00 -0500

    commit

---
+++ Ruby_Basics/Return.rb
@@ -0,0 +1,62 @@
+# 1
+
+# Breakfast
+
+# 2
+
+# Evening. The return value is the last line of code executed.
+
+# 3
+
+# Breakfast. It will exit the method on line 2 because "return" is used, making 'Breakfast' the last line of code.
+
+# 4
+
+# Dinner
+# Breakfast
+# puts is used to output Dinner in the method, and then puts is used to print the return value of meal, which is Breakfast.
+
+# 5
+
+# Dinner
+# nil
+# There are two values because there is a puts and a p. Puts gives us "Dinner", and p gives us the return value, nil.
+
+# 6
+
+# Breakfast
+# Since return is used in the first line of the method, we exit the method after Breakfast.
+
+# 7
+
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+
+#8
+
+# 0
+# 1
+# 2
+# 3
+# 4
+# 10
+
+# 9
+
+# 0
+# 1
+# 2
+# nil
+# Once sheep equals 2, the method exits because of the return, and there is no value next to return, so there is a nil att the end.
+
+# 10
+
+# 1
+# The if statement will always evaluate true because it's just a variable assignment.
+
+
+
